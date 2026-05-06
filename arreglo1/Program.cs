caf3dd9179aa7226b1fe9c98323ac64f2e0dd910
luis <lcrodriguez@uamv.edu.ni>
2026-05-06 12:03:30 -0600

    feat: adad arreglo1 project

---
+++ arreglo1/Program.cs
@@ -0,0 +1,22 @@
+﻿//leer 5 edades y mostrar la edad promedio
+
+    
+        int[] edades = new int[5];
+        double suma = 0;
+double promedio= 0;
+
+        for (int i = 0; i < edades.Length; i++)
+       { 
+            Console.Write("dime la edad: " +(i + 1) + ": ");
+            edades[i] = int.Parse(Console.ReadLine()!);
+       }
+
+        for (int i = 0; i < edades.Length; i++)
+        {
+            suma += edades[i];
+        }
+
+        promedio = suma / edades.Length;
+
+        Console.WriteLine("La edad promedio es: " + promedio);
+         
